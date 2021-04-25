ed545ab3e13d8c5a88a87cd91fca80cfe3f87ea2
Vlad <berserk-li@mail.ru>
2021-04-25 17:21:32 +0300

    небольшой рефакторинг кода для дискорд-функций

--- utils/discord.js
+++ utils/discord.js
@@ -8,30 +8,39 @@ const {Discord, client, config} = _local
 
 
 module.exports = {
-    sendChannel,
-    sendUser,
-    superSendChannel,
-    superSendUser
+    sendToChannel,
+    sendToUser,
+    superSendToChannel,
+    superSendToUser,
+	deleteFromChannel
 }
 
 
 /**
  * отправляет сообщение на указанный канал
+ * @param {Number} chId - id канала на который будет отправленно сообщение
+ * @param {String || MessageEmbed} textMess - текст сообщения или обьект Embed
+ * @returns {Promise} - обьект класса канала дискорда на который было отправленно сообщение
  */
- function sendChannel(channelId, mess) {
-	return new Promise(resolve => {
-		client.channels.fetch(channelId)
+ function sendToChannel(chId, textMess) {
+	return new Promise((resolve, reject) => {
+		client.channels.fetch(chId)
 		.then(ch => {
-			ch.send(mess)
+			ch.send(textMess)
 			.then(() => {
-				return resolve({
-					status: true,
-					channel: ch
-				})
+				return resolve(ch)
 			})
 			.catch(err => {
-				console.log(err)
-				return resolve({status: false, err})
+				return reject({
+					err,
+					log_msg: `Ошибка отправки сообщения на указанный канал (${chId})`
+				})
+			})
+		})
+		.catch(err => {
+			return reject({
+				err,
+				log_msg: `Ошибка при получении канала по id (${chId})`
 			})
 		})
 	})
@@ -40,18 +49,29 @@ module.exports = {
 
 /**
  * отправляет сообщение пользователю
+ * @param {Number} userId - id пользователя
+ * @param {String || MessageEmbed} mess - текст сообщения или обьект Embed
+ * @returns {Promise} - обьект класса пользователя которому было отправленно сообщение
  */
- function sendUser(userId, mess) {
-	return new Promise(resolve => {
+ function sendToUser(userId, mess) {
+	return new Promise((resolve, reject) => {
 		client.users.fetch(userId)
-		.then(ch => {
-			ch.send(mess)
+		.then(user => {
+			user.send(mess)
 			.then(() => {
-				return resolve(true)
+				return resolve(user)
 			})
-			.catch(e => {
-				console.log(e)
-				return resolve(false)
+			.catch(err => {
+				return reject({
+					err,
+					log_msg: `Ошибка отправки сообщения пользователю (${userId})`
+				})
+			})
+		})
+		.catch(err => {
+			return reject({
+				err,
+				log_msg: `Ошибка при получении пользователя по id (${userId})`
 			})
 		})
 	})
@@ -62,14 +82,84 @@ module.exports = {
  * выполняет указанную команду и параметры от имени указанного пользователя отправляя результат на указанный канал
  * (что-то типо ручного ответа если вдруг первый раз оно выдало ошибку)
  */
-function superSendChannel() {}
+function superSendToChannel() {}
 
 
 /**
  * выполняет указанную команду и параметры от имени указанного пользователя отправляя результат ему же или другому пользователю
  * (что-то типо ручного ответа если вдруг первый раз оно выдало ошибку)
  */
-function superSendUser() {}
+function superSendToUser() {}
+
+
+/**
+ * удаляет сообщение из указанного канала
+ * @param {String} chId - id канала на котором будет происходить удаление
+ * @param {Number} messId - id сообщения
+ * @returns {Promise}
+ */
+function deleteFromChannel(chId, messId) {
+	return new Promise((resolve, reject) => {
+		client.channels.fetch(chId)
+		.then(ch => {
+			ch.messages.fetch(messId)
+			.then(m => {
+				m.delete()
+				.then(() => {
+					return resolve(true)
+				})
+				.catch(err => {
+					return reject({
+						err,
+						log_msg: `Ошибка удаления сообщения по id (${messId}) на канале (${chId})`
+					})
+				})
+			})
+			.catch(err => {
+				return reject({
+					err,
+					log_msg: `Ошибка при получении сообщения по id (${messId}) на канале (${chId})`
+				})
+			})
+		})
+		.catch(err => {
+			return reject({
+				err,
+				log_msg: `Ошибка при получении канала по id (${chId})`
+			})
+		})
+	})
+}
+
+
+/**
+ * удаляет сообщение из текущего канала
+ * @param {Number} messId - id сообщения
+ * @returns {Promise}
+ */
+Discord.Message.prototype.deleteById = function(messId) {
+	return new Promise((resolve, reject) => {
+		this.channel.messages.fetch(messId)
+		.then(m => {
+			m.delete()
+			.then(() => {
+				return resolve(true)
+			})
+			.catch(err => {
+				return reject({
+					err,
+					log_msg: `Ошибка удаления сообщения по id (${messId}) на канале (${this.channel.id})`
+				})
+			})
+		})
+		.catch(err => {
+			return reject({
+				err,
+				log_msg: `Ошибка при получении сообщения по id (${messId}) на канале (${this.channel.id})`
+			})
+		})
+	})
+}
 
 
 /**
@@ -83,7 +173,7 @@ Discord.Message.prototype.isOwner = function() {
 
 
 /**
- * првоеряет, достаточно ли прав у бота в этом канале
+ * проверяет, достаточно ли прав у бота в этом канале
  * @param {Array || String} perm - права которые нужны боту
  * @returns {Boolean}
  */
@@ -91,7 +181,6 @@ Discord.Message.prototype.hasPerm = function(perm) {
 	if ( !this.channel.permissionsFor ) return true // для DM - true
 	return this.channel.permissionsFor(client.user).has(perm)
 }
-
 
 
 /**

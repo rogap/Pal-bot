3270e1d4aabb359a52dce8da72c64cdd3a00444b
Vlad <berserk-li@mail.ru>
2021-04-26 12:43:52 +0300

    добавил обработку ошибок команды console

--- commands/console/execute.js
+++ commands/console/execute.js
@@ -2,18 +2,36 @@
  * функция которая выполняет комнаду и отправляет результат пользователю
  */
 
+const { resolve } = require("path")
+
 
 const _local = process._local
 const {client, config, champions, usersSettings, guildsSettings, commands, utils} = _local
-const {sendUser, sendChannel} = utils
+const {sendToChannel, sendToUser, superSendToChannel, superSendToUser, deleteFromChannel} = utils
 
 
 module.exports = function(message, settings, command) {
-    const content = message.parseContent().cut(settings.prefix)
-    const cont = command.getContent(content)
-    try {
-        eval(cont)
-    } catch(e) {
-        console.log(e)
-    }
+    return new Promise((resolve, reject => {
+        const content = message.parseContent().cut(settings.prefix)
+        const cont = command.getContent(content)
+        try {
+            const result = eval(cont)
+            if ( result !== null && result !== undefined && result.constructor === Promise ) {
+                result.then(some => {
+                    return resolve(some)
+                })
+                .catch(err => {
+                    return reject({
+                        err
+                    })
+                })
+            }
+
+            return resolve(result)
+        } catch(err) {
+            return reject({
+                err
+            })
+        }
+    }))
 }

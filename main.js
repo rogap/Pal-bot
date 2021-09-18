01e2f49797a7a48df3472e02abfcd2b2af304cae
Qu Dey <berserk-li@mail.ru>
2021-09-18 09:54:09 +0300

    fix

--- main.js
+++ main.js
@@ -7,7 +7,6 @@ const Discord = require('discord.js')
 const Intents = Discord.Intents
 const client = new Discord.Client({ intents: [
     Intents.FLAGS.GUILDS,
-    Intents.FLAGS.GUILD_MEMBERS,
     Intents.FLAGS.GUILD_INTEGRATIONS,
     Intents.FLAGS.GUILD_MESSAGES,
     Intents.FLAGS.GUILD_MESSAGE_TYPING,
